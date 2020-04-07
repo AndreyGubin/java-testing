500c18c24f23a6bde9448fb548e1ccb43cd9cd08
Andrey Gubin <help@terrazor.com>
2020-04-07 14:20:32 +0400

    Исравлен файл TestBase

--- addressbook-web-tests/src/test/java/com/terrazor/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/terrazor/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package com.terrazor.addressbook.tests;
 
 import com.terrazor.addressbook.appmanager.ApplicationManager;
+import com.terrazor.addressbook.model.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
@@ -41,5 +42,58 @@ public class TestBase {
         } catch (NoAlertPresentException e) {
             return false;
         }
+    }
+
+    protected void selectContact() {
+        wd.findElement(By.name("selected[]")).click();
+    }
+
+    protected void deleteContact() {
+        wd.findElement(By.xpath("//input[@value='Delete']")).click();
+    }
+
+    protected void acceptDeletion() {
+        wd.switchTo().alert().accept();
+    }
+
+    protected void editContact() {
+        wd.findElement(By.xpath("//img[@alt='Edit']")).click();
+    }
+
+    protected void saveContactEdition() {
+        wd.findElement(By.name("update")).click();
+    }
+
+    protected void editContactAddress() {
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys("Russia");
+    }
+
+    protected void returnToHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitContactCreation() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    protected void fillContactPage(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobileNumber());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    protected void gotoAddContactPage() {
+        wd.findElement(By.linkText("add new")).click();
     }
 }

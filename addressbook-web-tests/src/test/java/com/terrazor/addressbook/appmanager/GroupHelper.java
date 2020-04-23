51efa0c21cc8782428bcfb3eb0ac37942ed1d23b
AndreyGubin <mail@terrazor.com>
2020-04-23 16:24:29 +0400

    Формирование коллекции

--- addressbook-web-tests/src/test/java/com/terrazor/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/com/terrazor/addressbook/appmanager/GroupHelper.java
@@ -3,6 +3,10 @@ package com.terrazor.addressbook.appmanager;
 import com.terrazor.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends BaseHelper {
 
@@ -70,4 +74,15 @@ public class GroupHelper extends BaseHelper {
     }
 
 
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+        }
+
+        return groups;
+    }
 }

fdf46684c004e803b4be80773c430ee66ac2a211
AndreyGubin <mail@terrazor.com>
2020-05-05 16:46:11 +0400

    Задание 13

--- addressbook-web-tests/src/test/java/com/terrazor/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/com/terrazor/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,51 @@
 package com.terrazor.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.terrazor.addressbook.model.ContactData;
 import com.terrazor.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.sql.SQLOutput;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
-        app.getContactHelper().goToHomePage();
-        Contacts before = app.getContactHelper().all();
-        File photo = new File("src/test/java/com/terrazor/addressbook/resources/pic.jpg");
-        ContactData contact = new ContactData().withFirstName("Andrey").withLastName("Gubin").withPhoto(photo).withGroup("test1").withHomePhone("123").withMobilePhone("456").withWorkPhone("789");
-        app.getContactHelper().createContact(contact, true);
-        Contacts after = app.getContactHelper().all();
-        assertThat(after.size(), equalTo(before.size() + 1));
-        contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
-        assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/java/com/terrazor/addressbook/resources/contacts.json"));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
     }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
+            app.getContactHelper().goToHomePage();
+            Contacts before = app.getContactHelper().all();
+            app.getContactHelper().createContact(contact, true);
+            assertThat(app.getContactHelper().getContactCount(), equalTo(before.size() + 1));
+            Contacts after = app.getContactHelper().all();
+            assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        }
+
 
     @Test (enabled = false)
     public void testCurrentDir() {

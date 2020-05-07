9a1beed0c02f4925423a4dc738f94b1f98ef7863
AndreyGubin <mail@terrazor.com>
2020-05-07 22:02:24 +0400

    Задание 8.5

--- mantis-tests/src/test/java/com/terrazor/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/com/terrazor/mantis/tests/TestBase.java
@@ -5,18 +5,25 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeSuite(alwaysRun = true)
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() { app.stop(); }
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
+        app.stop();
+    }
 
 }
 
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = 'root';
+$g_signup_use_captcha = OFF;
+
+$g_default_timezone = 'UTC';
+
+$g_crypto_master_salt = '2tmolpLdU2iiWEWYrBxpaNizx63h1qFsEBgpCXmhQ04=';
